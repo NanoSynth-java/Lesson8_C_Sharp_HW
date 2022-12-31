34c9175fbca6d9b6c1fdb7ca7bcd7e73803eaeee
Daniil <Comm2k3@hotmail.com>
2022-12-31 03:26:04 +0200

    4th is done

--- Program.cs
+++ Program.cs
@@ -144,48 +144,111 @@
 // 15 18
 
 
-int[,] arrayA = {
-    {2,4},
-    {3,2}
-};
-int[,] arrayB = {
-    {3,4},
-    {3,3}
-};
+// int[,] arrayA = {
+//     {2,4},
+//     {3,2}
+// };
+// int[,] arrayB = {
+//     {3,4},
+//     {3,3}
+// };
 
-void PrintArray(int[,] array)
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + "\t");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// PrintArray(arrayA);
+// PrintArray(arrayB);
+
+// int [,] result = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+
+// int[,] TwoMatrice(int[,] arrayA, int[,] arrayB)
+// {
+
+//     for (int i = 0; i < arrayA.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arrayB.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < arrayA.GetLength(1); k++)
+//             {
+//                 result[i, j] += arrayA[i, k] * arrayB[k, j];
+
+//             }
+//         }
+//     }
+//     return result;
+// }
+// TwoMatrice(arrayA, arrayB);
+// PrintArray(result);
+
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.WriteLine("Сколько будет строк ? ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сколько будет столбцов ? ");
+int cols = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Какой будет глубина трехмерности массива ;) ?"); // ничего смешнее придумать и понятнее я придумать не смог.
+int third = Convert.ToInt32(Console.ReadLine());
+int[,,] array3d = new int[rows, cols, third];
+Random rndnum = new Random();
+
+// int[,,] FillArray(int[,,] array3d, int min, int max)
+// {
+//     foreach (int rows in array3d)
+//     {
+//         foreach (int cols in array3d)
+//         {
+//             foreach (int third in array3d)
+//             {
+//                 array3d[rows,cols,third] = rndnum.Next(min, max + 1);
+//             }
+//         }
+//     }
+//     return array3d;
+// }
+
+int [,,] FillArray(int[,,] array3d, int min, int max)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array3d.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < array3d.GetLength(1); j++)
         {
-            Console.Write(array[i, j] + "\t");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
-
-PrintArray(arrayA);
-PrintArray(arrayB);
-
-int [,] result = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
-
-int[,] TwoMatrice(int[,] arrayA, int[,] arrayB)
-{
-
-    for (int i = 0; i < arrayA.GetLength(0); i++)
-    {
-        for (int j = 0; j < arrayB.GetLength(1); j++)
-        {
-            for (int k = 0; k < arrayA.GetLength(1); k++)
+            for (int k = 0; k < array3d.GetLength(2); k++)
             {
-                result[i, j] += arrayA[i, k] * arrayB[k, j];
-
+                array3d[i,j,k] = rndnum.Next(min,max + 1);
             }
         }
     }
-    return result;
+    return array3d;
 }
-TwoMatrice(arrayA, arrayB);
-PrintArray(result);
+
+void PrintArray(int[,,] array3d)
+{
+    for (int i = 0; i < array3d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3d.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3d.GetLength(2); k++)
+            {
+                Console.Write($"row {i} column {j} 3d {k} (Элемент хранит число {array3d[i,j,k]}) \t");
+            }
+        } Console.WriteLine();
+    } Console.WriteLine();
+}
+
+FillArray(array3d,1,10);
+PrintArray(array3d);
